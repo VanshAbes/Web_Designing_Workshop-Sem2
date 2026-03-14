ea8ebce8d740e7d8356bc0055b7287703e1817d5
VanshAbes <vansh.25b01010066@abes.ac.in>
2026-03-14 15:59:56 +0530

    Working with JavaScript

---
+++ javascript/script.js
@@ -0,0 +1,84 @@
+// alert("Hello Student");
+// console.log("JavaScript is running");
+
+// let Name = "Vansh Saxena";
+// let age = 18;
+// let phone_number = 9012562896;
+
+// console.log(Name);
+// console.log(age);
+// console.log(phone_number);
+
+// document.getElementById("mySubmit").onclick = function(){
+//     username = document.getElementById("mytext").value;
+//     document.getElementById("myh1").textContent = `Hello ${username}`;
+// }
+
+// let marks = 75;
+// if (marks>50){
+//     console.log("Pass");
+// }
+// else{
+//     console.log("Fail");
+// }
+
+let num = 16;
+// while(num>0){
+//     if(num%2!=0){
+//         console.log(num);
+//     }
+//     num=num-1;
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//         row += " * ";
+//     }
+//     console.log(row);
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     let row = "";
+//     for (let j = 4; j >= i; j--) {
+//         row += " * ";
+//     }
+//     console.log(row);
+// }
+
+//Arrow Function: it provide a  shorter syntax of the function
+
+// traditional function
+// function add(a,b){
+//     return a+b;
+// }
+
+//arrow function
+
+// const add = (a,b) => a+b;
+// console.log(add(5,3));
+
+
+/*
+map(): create a new array by transformijg each element
+
+let numbers = [1,2,3,4];
+let square = numbers.map(num => num*num);
+console.log(square);
+*/
+
+/*
+filter(): create a new array with elements that satisfy a condition
+
+let numbers =[10,15,20,25];
+let result = numbers.filter(num => num>15 );
+console.log(result);
+*/
+
+/*
+reduce(): reduce array to a single value
+
+let numbers = [10,20,30];
+let sum = numbers.reduce((total,num) => total + num , 0);
+console.log(sum);
+*/
